67e9c4af2000a0c3a7409acb520ad2b10f26155f
Andrea <andrea.castegnaro@outlook.com>
2024-11-13 15:14:58 +0000

    Changed savetofigurefile function

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,11 +1,13 @@
 import os
 import matplotlib.pyplot as plt
 
-def savefiguretofile(filename, directory, dpi, transparent=False):
+def savefiguretofile(filename, directory, dpi=300, transparent=False, png_only=True):
     filename_pdf = os.path.join(directory, f"{filename}.pdf")
     filename_png = os.path.join(directory, f"{filename}.png")
     filename_svg = os.path.join(directory, f"{filename}.svg")
 
-    plt.savefig(filename_pdf, format='pdf', dpi=dpi, bbox_inches='tight', transparent=transparent)
+    if not png_only:
+        plt.savefig(filename_pdf, format='pdf', dpi=dpi, bbox_inches='tight', transparent=transparent)
+        plt.savefig(filename_svg, format='svg', dpi=dpi, bbox_inches='tight', transparent=transparent)
+        
     plt.savefig(filename_png, format='png', dpi=dpi, bbox_inches='tight', transparent=transparent)
-    plt.savefig(filename_svg, format='svg', dpi=dpi, bbox_inches='tight', transparent=transparent)
